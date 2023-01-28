31fcaab6d61bde570dda8e282d5a5d9e727a6400
Juan Camilo <caamilo9517@gmail.com>
2023-01-27 22:43:15 -0500

    Realizando ejustes al código

--- js/login.js
+++ js/login.js
@@ -1,22 +1,29 @@
+const usuarios= [
+  { cargo: 'secretary', clave: '1234' },
+  { cargo: 'vendedor', clave: '5678' },
+  { cargo: 'ensamblador', clave: '9012' }
+];
 const login = Vue.createApp({
     data() {
       return {
         cargo: '',
         clave: '',
-        usuarios: [
-          { cargo: 'secretary', clave: '1234' },
-          { cargo: 'vendedor', clave: '5678' },
-          { cargo: 'ensamblador', clave: '9012' }
-        ]
+        login:true
+        
       }
     },
     methods: {
       enviar() {
+        if(this.cargo==="" && this.clave==="") {
+          alert('Recuerde llenar todos los campos, por favor intente nuevamente');
+          return
+        }
         // Validar el cargo y la clave aquí
-        for (let i = 0; i < this.usuarios.length; i++) {
-          const usuario = this.usuarios[i];
+        for (let i = 0; i < usuarios.length; i++) {
+          const usuario = usuarios[i];
           if (this.cargo === usuario.cargo && this.clave === usuario.clave) {
             alert('¡Éxito! Has ingresado con éxito.');
+            this.login=false; //Esto es para que desaparezca la sesión del login.
             return;
           }
         }
@@ -25,3 +32,5 @@ const login = Vue.createApp({
       }
     }
   });
+
+  login.mount(".login")
