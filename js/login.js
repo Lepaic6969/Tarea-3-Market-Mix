60f61c9334840cd1adcdda62730c7ce865c1dc35
juans <juansmgb@gmail.com>
2023-01-27 21:29:20 -0500

    creacion de formulario y validacion de usuario

--- js/login.js
+++ js/login.js
@@ -1,13 +1,27 @@
-const login= Vue.createApp({
-    data(){
-        return {
-
-        }
+const login = Vue.createApp({
+    data() {
+      return {
+        cargo: '',
+        clave: '',
+        usuarios: [
+          { cargo: 'secretary', clave: '1234' },
+          { cargo: 'vendedor', clave: '5678' },
+          { cargo: 'ensamblador', clave: '9012' }
+        ]
+      }
     },
-    methods:{
-
+    methods: {
+      enviar() {
+        // Validar el cargo y la clave aquí
+        for (let i = 0; i < this.usuarios.length; i++) {
+          const usuario = this.usuarios[i];
+          if (this.cargo === usuario.cargo && this.clave === usuario.clave) {
+            alert('¡Éxito! Has ingresado con éxito.');
+            return;
+          }
+        }
+  
+        alert('¡Error! El cargo o la clave son incorrectos.');
+      }
     }
-})
-
-
-login.mount(".login");
+  });
