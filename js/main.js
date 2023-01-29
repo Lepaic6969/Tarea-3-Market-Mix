ff6ca7b1557ed42b0f8f6414d4c489e4c8b87574
Juan Camilo <caamilo9517@gmail.com>
2023-01-29 12:57:43 -0500

    Componente admin funcional

--- js/main.js
+++ js/main.js
@@ -12,16 +12,18 @@ const main = Vue.createApp({
         clave: '',
         loginExitoso:false,
         direccionamiento:"",
-        //Variables Admin
+
+        //Variables que controla el Admin
         cargoAdmin:'',
-        baseSecretario:1160000, //Salario base de cada una de las profesiones.
-        baseVendedor:1500000,
+        baseSecretario:1160000, //Salario base secretario
+        baseVendedor:1500000, //Salario base vendedor
         baseEnsamblador:1500000,
-        maxZapatos:3000,
+        comisionVendedor:5, // Comisión del vendedor => 5% del total de sus ventas
+        maxZapatos:2000,
         maxZapatillas:4000,
         bonoEnsambleZapatos:1000,
         bonoEnsambleZapatillas:700,
-        comisionVendedor:5 // 5% del total de sus ventas
+        variableEnsamblador:''
 
 
       }
@@ -59,20 +61,19 @@ const main = Vue.createApp({
       /******************************************************************************************** */
       //ESTOS SON MÉTODOS DEL ADMINISTRADOR.
       validarSalario(salario,cargo){
-           let validacionExitosa=true;
            if(salario===""){
             alert("Recuerde diligenciar la cantidad que quiere modificar");
-            validacionExitosa=false;
+            return false;
           }
           if(salario<1160000){
             alert(`El salario del ${cargo}, no puede ser inferior al salario mínimo`);
-            validacionExitosa=false;
+            return false;
           }
           if(salario>2500000){
             alert(`El sueldo del ${cargo}, no puede ser superior a 2'500.000`);
-            validacionExitosa=false;
+            return false;
           }
-          return validacionExitosa;
+          return true;
       },
       validarComision(){
         let validacionExitosa=true;
@@ -80,10 +81,14 @@ const main = Vue.createApp({
             alert("Recuerde que la comisión no puede sobrepasar el 20% ni tener un porcentaje negativo.");
             validacionExitosa=false;
           }
+          if(this.comisionVendedor===''){
+            alert("Recuerde llenar el campo de la comisión.");
+            validacionExitosa=false;
+          }
           return validacionExitosa;
       },
       modificarSecretario(){
-        if(this.baseSecretario===1160000) return; //Este es para que no se haga nada en caso de que el valor ingresado seal el mismo que ya estaba.
+        // if(this.baseSecretario===1160000) return; //Este es para que no se haga nada en caso de que el valor ingresado seal el mismo que ya estaba.
         let datosCorrectos=this.validarSalario(this.baseSecretario,'secretario');
         if(datosCorrectos){
           alert(`Modificación salarial exitosa, el nuevo salario base del secretario es de ${this.baseSecretario}`);
@@ -108,14 +113,55 @@ const main = Vue.createApp({
         }
         
       },
-      modificarEnsamblador(){
-        if(this.baseEnsamblador===1500000) return; 
+      modificarEnsamblador1(){
+
+        // if(this.baseEnsamblador===1500000) return; 
         let datosCorrectos=this.validarSalario(this.baseEnsamblador,'ensamblador');
         if(datosCorrectos){
           alert(`Modificación salarial exitosa, el nuevo salario base del ensamblador es de ${this.baseEnsamblador}`);
         }else{
           this.baseEnsamblador=1500000;
         }
+      },
+      modificarEnsamblador2(){
+        // if(this.maxZapatos===3000 || this.maxZapatos===4000) return;
+        if(this.maxZapatos==="" || this.maxZapatos===""){
+          alert("Recuerde llenar todos los campos")
+          // this.maxZapatos===2000; 
+          // this.maxZapatillas===4000;
+          return
+        }
+        if(!(this.maxZapatos>0 && this.maxZapatos<4000)){
+          alert("Recuerde que la cantidad máxima de zapatos dede estar entre 0 y 4000 pares")
+          this.maxZapatos=3000;
+          return
+        }
+        if(!(this.maxZapatillas>0 && this.maxZapatillas<6000)){
+          alert("Recuerde que la cantidad máxima de zapatillas dede estar entre 0 y 6000 pares")
+          this.maxZapatillas=4000;
+          return
+        }
+        alert("Modificación exitosa");
+      },
+      modificarEnsamblador3(){
+        // Esta primera validación me está fallando sólo en este componente.
+        // if(this.bonoEnsambleZapatos===1000 || this.bonoEnsambleZapatillas===700) return;
+        if(this.bonoEnsambleZapatos==="" || this.bonoEnsambleZapatillas===""){
+          alert("Recuerde llenar todos los campos")
+          return
+        }
+       
+        if(!(this.bonoEnsambleZapatos>=0 && this.bonoEnsambleZapatos<3000)){
+          alert("Recuerde que el bono por ensamble de zapatos dede estar entre 0 y 3000 pesos")
+          this.bonoEnsambleZapatos=1000;
+          return
+        }
+        if(!(this.bonoEnsambleZapatillas>=0 && this.bonoEnsambleZapatillas<2000)){
+          alert("Recuerde que el bono por ensamble de zapatillas dede estar entre 0 y 2000 pesos")
+          this.bonoEnsambleZapatillas=700;
+          return
+        }
+        alert("Modificación exitosa");
       }
 
     }
